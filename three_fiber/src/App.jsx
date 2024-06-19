9130a5a798d7611ea8846ab2496a0309969b41a8
unknown <cocfanlike@gmail.com>
2024-06-19 17:10:48 +0545

    Added Animation

--- three_fiber/src/App.jsx
+++ three_fiber/src/App.jsx
@@ -1,8 +1,19 @@
-import {Canvas} from '@react-three/fiber'
+import {Canvas,useFrame} from '@react-three/fiber'
+import {useRef} from 'react'
 
 const Cube = ({position, size, color}) => {
+
+  const ref = useRef()
+  
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta
+    ref.current.rotation.y -= delta
+    ref.current.position.z = Math.cos(state.clock.elapsedTime)*2
+    console.log(state.clock.elapsedTime)
+    
+  })
   return (
-    <mesh position={position}>
+    <mesh position={position} ref={ref}>
             <boxGeometry args={size}/>
             <meshStandardMaterial color={color}  />
           </mesh>
@@ -20,12 +31,14 @@ function App() {
       <>
       <div className="h-screen flex items-center justify-center">
         <Canvas >
-          {/* <ambientLight intensity={8}/> */}
+           <ambientLight intensity={0.5}/> 
           <directionalLight  position={[0,0,2]}/>
+          <group position={[0,-1,0]}>
           <Cube position={[1,0,0]} color="red"/>
           <Cube position={[-1,0,0]}  size={[1,1,1]}color="blue"/>
           <Cube position={[1,2,0]} color="yellow"/>
           <Cube position={[-1,2,0]} color="green"/>
+          </group>
 
         </Canvas>
       </div>
